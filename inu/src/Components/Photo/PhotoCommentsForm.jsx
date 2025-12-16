8f27f38d5d68f516cb00fba299709d8b31bb21dd
Allan <allan.tkm0@gmail.com>
2025-12-16 16:18:40 -0300

    Comentários

--- inu/src/Components/Photo/PhotoCommentsForm.jsx
+++ inu/src/Components/Photo/PhotoCommentsForm.jsx
@@ -7,7 +7,7 @@ import styles from './PhotoCommentsForm.module.css';
 
 
 
-const PhotoCommentsForm = ({id, setComments}) => {
+const PhotoCommentsForm = ({id, setComments, single }) => {
     const [comment, setComment] = React.useState('');
     const {request, error} = useFetch();
 
@@ -22,7 +22,7 @@ const PhotoCommentsForm = ({id, setComments}) => {
     }
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
+    <form className={`${single ? styles.single : '' } ${styles.form}`} onSubmit={handleSubmit}>
         <textarea
             className={styles.textarea}
             id="comment"
